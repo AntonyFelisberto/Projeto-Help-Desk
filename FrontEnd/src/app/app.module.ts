4e0eb272c18adab18aaf35cf3ac9323090766fd5
Antony dos santos felisberto <88542984+AntonyFelisberto@users.noreply.github.com>
2022-10-29 14:00:42 -0300

    adicionando dependencias do front end

--- FrontEnd/src/app/app.module.ts
+++ FrontEnd/src/app/app.module.ts
@@ -3,14 +3,51 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+//PARA TRABALHAR COM FORMULARIOS
+import { FormsModule , ReactiveFormsModule } from '@angular/forms';
+
+//PARA FAZER REQUISIÇÕES HTTP
+import {HttpClientModule} from '@angular/common/http'
+
+//ANGULAR MATERIAL
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatPaginatorModule} from '@angular/material/paginator'
+import {MatSnackBarModule} from '@angular/material/snack-bar'
+import {MatCheckboxModule} from '@angular/material/checkbox'
+import {MatToolbarModule} from '@angular/material/toolbar'
+import {MatSidenavModule} from '@angular/material/sidenav'
+import {MatButtonModule} from '@angular/material/button';
+import {MatSelectModule} from '@angular/material/select'
+import {MatTableModule} from '@angular/material/table'
+import {MatRadioModule} from '@angular/material/radio'
+import {MatInputModule} from '@angular/material/input'
+import {MatIconModule} from '@angular/material/icon'
+import {MatListModule} from '@angular/material/list'
+import {MatCardModule} from '@angular/material/card'
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatPaginatorModule,
+    MatSnackBarModule,
+    MatCheckboxModule,
+    MatToolbarModule,
+    MatSidenavModule,
+    MatSelectModule,
+    MatTableModule,
+    MatRadioModule,
+    MatInputModule,
+    MatIconModule,
+    MatListModule,
+    MatCardModule
   ],
   providers: [],
   bootstrap: [AppComponent]

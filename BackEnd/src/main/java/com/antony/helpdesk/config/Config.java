1f3611aeb87381fa044cfdcd836e343f078c299d
Antony dos santos felisberto <88542984+AntonyFelisberto@users.noreply.github.com>
2022-11-02 14:38:22 -0300

    retirando propriedades para começar implementação

--- BackEnd/src/main/java/com/antony/helpdesk/config/Config.java
+++ BackEnd/src/main/java/com/antony/helpdesk/config/Config.java
@@ -2,7 +2,6 @@ package com.antony.helpdesk.config;
 
 import com.antony.helpdesk.services.DataBaseServices;
 import lombok.AllArgsConstructor;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
@@ -15,15 +14,9 @@ public class Config {
 
     private DataBaseServices dataBaseServices;
 
-    @Value("${spring.jpa.hibernate.ddl-auto}")
-    private String value;
-
     @Bean
-    public boolean intanciarData(){
-        if(value.equals("create") || value.equals("update")){
-            dataBaseServices.intanciarData();
-        }
-        return false;
+    public void intanciarData(){
+        dataBaseServices.intanciarData();
     }
 
 }

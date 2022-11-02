a336f7eb823f58492a4e1d7f3317cf363c1c3afd
Antony dos santos felisberto <88542984+AntonyFelisberto@users.noreply.github.com>
2022-11-02 13:30:52 -0300

    iniciando services

---
+++ BackEnd/src/main/java/com/antony/helpdesk/config/Config.java
@@ -0,0 +1,9 @@
+package com.antony.helpdesk.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+@Configuration
+@Profile("dev")
+public class Config {
+}
---
+++ BackEnd/src/main/java/com/antony/helpdesk/services/DataBaseServices.java
@@ -0,0 +1,12 @@
+package com.antony.helpdesk.services;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class DataBaseServices {
+
+    public void instanceDb(){
+
+    }
+
+}
